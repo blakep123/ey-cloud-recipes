6c40fe7f4a26f95e0caba52c370b7e593f64962f
B. Poutra <rubyMac@B-Poutras-MacBook.local>
2010-06-17 07:41:48 -0500

    pushing changes to git for ey cloud recipes

--- cookbooks/ssmtp/recipes/default.rb
+++ cookbooks/ssmtp/recipes/default.rb
@@ -18,9 +18,26 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
     action :create
     not_if "test -d /data/ssmtp"
   end
+ 
 
   link "/etc/ssmtp" do
     to '/data/ssmtp'
+  end
+  
+  template "/etc/ssmtp/ssmtp.conf" do
+    owner 'deploy'
+    group 'deploy'
+    mode  0644
+    source "ssmtp.conf.erb"
+    variables ({
+      :server => "smtp.gmail.com",
+      :port => "587",
+      :domain => "youdesignit.com",
+      :fromLineOverrides =>"YES",
+      :UseSTARTTLS => "YES",
+      :AuthUser => "no-reply@youdesignit.com",
+      :AuthPass => "ydinoreply"
+      })
   end
 
 end
