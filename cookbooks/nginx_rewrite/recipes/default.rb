242dc7816152bf298b0b3defcb5ef23ff7b64741
B. Poutra <rubyMac@B-Poutras-MacBook.local>
2010-12-03 17:17:16 -0600

    fixed typos

--- cookbooks/nginx_rewrite/recipes/default.rb
+++ cookbooks/nginx_rewrite/recipes/default.rb
@@ -4,8 +4,10 @@ require 'pp'
 # Recipe:: default
 #
 
-if ['solo', 'app', 'app_master'].include?(node[:instance_role])]
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   node[:applications].each do |app_name, data| 
+    
+    
     template "/etc/nginx/servers/#{app_name}.rewrites" do
       owner 'deploy'
       group 'deploy'
