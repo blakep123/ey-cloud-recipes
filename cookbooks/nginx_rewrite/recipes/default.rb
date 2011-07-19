e2b403b4b04a9941e94cb786b7149ead1c7df614
Blake Poutra <blake@youdesignit.com>
2011-07-19 11:06:41 -0500

    changed back to the if but put it in the right custom conf file

--- cookbooks/nginx_rewrite/recipes/default.rb
+++ cookbooks/nginx_rewrite/recipes/default.rb
@@ -6,16 +6,16 @@ require 'pp'
 
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   node[:applications].each do |app_name, data|   
-    template "/etc/nginx/servers/#{app_name}.custom.conf" do
+    template "/etc/nginx/servers/#{app_name}/custom.conf" do
       owner 'deploy'
       group 'deploy'
       mode  0644
-      source "server.conf.erb"
+      source "default.rewrites.erb"
     end
   end
   
   execute "restarting-nginx" do
-    command "/etc/init.d/nginx restart"
+    command "sudo /etc/init.d/nginx restart"
   end
   
   
